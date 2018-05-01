e20ebca3c39b0c912d8f3fdcce8e6bd52ded647b
anguspettifer <angus.pettifer@gmail.com>
2018-05-01 11:50:05 +0100

    update

--- directory.rb
+++ directory.rb
@@ -1,15 +1,25 @@
-student_count = 11
-puts "The students of Villians Academy"
+#array time
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
+
+# and time to print
+
+puts "The students of Villains Academy"
 puts "----------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-puts "Overall, we have #{student_count} great students"
+students.each do |student|
+  puts student
+end
+
+# print the total
+puts "Overall, we have #{students.count} great students"

0e7e2b14dcc7aa1dfd964300308d2560f4201ac6
anguspettifer <angus.pettifer@gmail.com>
2018-05-02 08:24:33 +0100

    added interactive menu

--- directory.rb
+++ directory.rb
@@ -53,9 +53,10 @@ end
 
 def print(students)
   cohort_array = students.map {|hash| hash[:cohort]}.uniq
-  puts "the list of cohorts is #{cohort_array}"
+  puts "The list of cohorts is #{cohort_array}".center(50)
   for i in 0...cohort_array.size
-    puts cohort_array[i]
+    puts ""
+    puts "#{cohort_array[i]}:"
     students.each do |hash|
       next if hash[:cohort] != cohort_array[i]
       puts hash[:name]
@@ -71,7 +72,26 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students".center(50)
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
+interactive_menu
